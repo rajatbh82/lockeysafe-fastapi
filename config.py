1bbb86427fc058b0b80432a7af48bcc6cf023c27
Manas Kaul <35189191+manaskaul@users.noreply.github.com>
2024-03-17 21:54:41 +0530

    added fix for CORS (#4)
    Co-authored-by: Manas Kaul <vause@Manass-MacBook-Air.local>

---
+++ config.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+]
+
+def setConfig(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
